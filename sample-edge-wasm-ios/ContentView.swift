7301207748b6ff24ed6b52f7cd63837c4c5faf4e
shotastage <hornet.live.mf@gmail.com>
2022-11-17 01:25:45 +0900

    add sample wasm executer

--- sample-edge-wasm-ios/ContentView.swift
+++ sample-edge-wasm-ios/ContentView.swift
@@ -6,14 +6,41 @@
 //
 
 import SwiftUI
+import WasmInterpreter
+
+
+struct WASMContainer {
+    private let _vm: WasmInterpreter
+
+    init() throws {
+        _vm = try WasmInterpreter(module: WASMContainer.wasm)
+    }
+
+    func add(_ first: Int, _ second: Int) throws -> Int {
+        Int(try _vm.call("add", Int32(first), Int32(second)) as Int32)
+    }
+
+    private static var wasm: [UInt8] {
+        let base64 = "AGFzbQEAAAABBwFgAn9/AX8DAgEABwcBA2FkZAAACgkBBwAgACABags="
+        return [UInt8](Data(base64Encoded: base64)!)
+    }
+}
 
 struct ContentView: View {
+    
+    let mod = try! WASMContainer()
+
+    @State var res = 0
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Result: \(res)")
+            Button("Run WASM") {
+                res = try! mod.add(1, 1)
+            }
         }
         .padding()
     }
